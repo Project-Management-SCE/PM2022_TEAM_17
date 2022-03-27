c4ba2bfb8c82bb6eb6295efa7af957ff34a7f4dd
Max Shapira <maximsh4@ac.sce.ac.il>
2022-03-27 20:16:19 +0300

    BSPM2022T17-31 created sign up form and sign up page for signing up agents

---
+++ StockFlow/AgentSignUp/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import CreateNewAgent
+
+# Create your views here.
+
+def AgentSignUp(response):
+    if response.method == "POST":
+        form = CreateNewAgent(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/home")
+    else:
+        form = CreateNewAgent()
+
+    return render(response, "AgentSignUp/signup_page.html", {"form":form})
+
+def home(response):
+    return HttpResponse("<h1>Test</h1>")
