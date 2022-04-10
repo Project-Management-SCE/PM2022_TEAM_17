97522fe26890cd54b81af118c54b45c5a6461a05
Evghenii <evghega@ac.sce.ac.il>
2022-04-11 01:01:28 +0300

    agent db corrected

--- StockFlow/AgentSignUp/admin.py
+++ StockFlow/AgentSignUp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Agent
+from .models import Agent, Customer
+from .models import Customer
 # Register your models here.
 
 admin.site.register(Agent)
+admin.site.register(Customer)
