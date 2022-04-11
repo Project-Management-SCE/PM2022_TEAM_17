8cddcc3136a7e72e4ef75d85c0661fda3a063a51
Max Shapira <maximsh4@ac.sce.ac.il>
2022-04-11 11:34:29 +0300

    Updated Agent Sign up page with more fields and different style

--- StockFlow/AgentSignUp/models.py
+++ StockFlow/AgentSignUp/models.py
@@ -5,17 +5,6 @@ from django.forms import BooleanField, CharField, IntegerField
 
 # Create your models here.
 
-# class Agent(models.Model):
-#     emp_ID = models.AutoField(primary_key=True)
-#     email = models.EmailField(max_length=50)
-#     password = CharField(min_length=6, max_length=40)
-#     full_name = CharField(max_length=80, required=True)
-#     city = CharField(max_length=50)
-#     Mobile = CharField(min_length=10, max_length=10)
-#     isAgent = BooleanField(initial=False)
-
-#     def __str__(self):
-#         return self.full_name
 
 class Agent(models.Model):
     emp_ID = models.AutoField(primary_key=True)
