8282dd3241bb5c4bf554c5204fab5ae6082e7173
Max Shapira <73330569+MaximSh4@users.noreply.github.com>
2022-04-15 01:44:30 +0300

    deleted crispy_forms
    deleted crispy_forms from settings file as we don't use it anymore

--- StockFlow/StockFlow/settings.py
+++ StockFlow/StockFlow/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AgentSignUp.apps.AgentsignupConfig',
-    'crispy_forms',
     'django_jenkins',
     'accounts',
 ]
